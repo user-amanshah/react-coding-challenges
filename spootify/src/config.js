3a8c3a7e72eb6607271dd0a56e96fc10df199e84
Aman Shah <59494960+user-amanshah@users.noreply.github.com>
2021-01-23 21:56:47 -0500

    remove credentials

--- spootify/src/config.js
+++ spootify/src/config.js
@@ -2,7 +2,7 @@ export default {
   api: {
     baseUrl: 'https://api.spotify.com/v1',
     authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: 'bdcf7eee3f3c40e6a51a794e1020ec0e',
-    clientSecret: '2a2af919d7e9445080fd692b26b6d52f'
+    clientId: '',
+    clientSecret: ''
   }
 }
